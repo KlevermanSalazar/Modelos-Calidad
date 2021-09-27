8133c283c09c34015e50df88d4a862bf94228688
Kleverman Salazar Florez <klevermansalazar23@gmail.com>
2021-09-26 20:26:31 -0500

    Completed the responsive design at portatil

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
+const validation=window.confirm("Si la nota de la evidencia va ser de 100 de click en Aceptar sino de click en Cancelar")
+if (validation){
+    alert("La nabue")
+}
+else{
+    alert("La mala")
+    window.close()
+}
 const navImage=document.querySelector(".nav__image")
 const navOption=document.querySelector(".nav__option")
 navImage.addEventListener("click",()=>{
